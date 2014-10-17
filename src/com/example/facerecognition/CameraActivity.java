d5f2cd93d873d18f589a07c3d9a85d103af36e76
Ge Liu <minoslg19910602@gmail.com>
2014-10-17 15:59:11 -0700

    A face recognition app in android paltform

---
+++ src/com/example/facerecognition/CameraActivity.java
@@ -0,0 +1,89 @@
+package com.example.facerecognition;
+
+import org.opencv.android.BaseLoaderCallback;
+import org.opencv.android.CameraBridgeViewBase;
+import org.opencv.android.LoaderCallbackInterface;
+import org.opencv.android.OpenCVLoader;
+import org.opencv.android.CameraBridgeViewBase.CvCameraViewFrame;
+import org.opencv.android.CameraBridgeViewBase.CvCameraViewListener2;
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.SurfaceView;
+import android.view.WindowManager;
+
+public class CameraActivity extends Activity implements CvCameraViewListener2{
+	private static final String TAG = "FaceRecognition::Activity";
+    
+	private CameraBridgeViewBase mOpenCvCameraView;
+	private Mat mRgba;
+	private Mat mGray;
+	
+	private BaseLoaderCallback  mLoaderCallback = new BaseLoaderCallback(this) {
+        @Override
+        public void onManagerConnected(int status) {
+            switch (status) {
+                case LoaderCallbackInterface.SUCCESS: 
+                {
+                	Log.i(TAG, "OpenCV loaded successfully");
+                    //System.loadLibrary("face_recognition");
+                } break;
+                default:
+                {
+                    super.onManagerConnected(status);
+                } break;
+            }
+        }
+    };
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+		setContentView(R.layout.activity_camera);
+		
+		mOpenCvCameraView = (CameraBridgeViewBase) findViewById(R.id.cameraView);
+		mOpenCvCameraView.setVisibility(SurfaceView.VISIBLE);
+		mOpenCvCameraView.setCvCameraViewListener(this);
+	}
+	
+	@Override
+	protected void onResume() {
+        super.onResume();
+        OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_2_4_9, this, mLoaderCallback);
+	}
+	
+	@Override
+	public void onPause() {
+		super.onPause();
+		if (mOpenCvCameraView != null)
+			mOpenCvCameraView.disableView();
+	}
+	
+	public void onDestroy() {
+        super.onDestroy();
+        if (mOpenCvCameraView != null)
+            mOpenCvCameraView.disableView();
+    }
+	
+	@Override
+	public void onCameraViewStarted(int width, int height) {
+		mRgba = new Mat(height, width, CvType.CV_8UC4);	
+		mGray = new Mat(height, width, CvType.CV_8UC1);
+	}
+
+	@Override
+	public void onCameraViewStopped() {
+		mRgba.release();
+		mGray.release();
+	}
+
+	@Override
+	public Mat onCameraFrame(CvCameraViewFrame inputFrame) {
+		mRgba = inputFrame.rgba();
+		return mRgba;
+	}
+}
